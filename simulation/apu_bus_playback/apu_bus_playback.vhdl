627328455347e2d13bcf789af48669819e03da64
aarmono <33583235+aarmono@users.noreply.github.com>
2022-10-16 13:12:52 -0700

    Update apu_bus_playback to not restart when file end reached

--- simulation/apu_bus_playback/apu_bus_playback.vhdl
+++ simulation/apu_bus_playback/apu_bus_playback.vhdl
@@ -22,8 +22,11 @@ architecture behavioral of apu_bus_playback is
 
     file fptr: text;
     
+    signal done_sig : boolean := false;
 begin
 
+    done <= done_sig;
+    
     process
         variable fstatus   : file_open_status;
         variable file_line : line;
@@ -33,28 +36,32 @@ begin
         variable bus_read  : boolean;
         variable bus_write : boolean;
     begin
-        done <= false;
-        file_open(fstatus, fptr, FILEPATH, read_mode);
-        
         apu_bus <= bus_idle(apu_bus);
         apu_data_out <= (others => '-');
-        while not endfile(fptr) loop
-            readline(fptr, file_line);
-            read(file_line, delta);
-            read(file_line, address);
-            read(file_line, bus_read);
-            read(file_line, bus_write);
-            read(file_line, data);
+        
+        if not done_sig
+        then
+            file_open(fstatus, fptr, FILEPATH, read_mode);
             
-            wait for delta;
+            while not endfile(fptr) loop
+                readline(fptr, file_line);
+                read(file_line, delta);
+                read(file_line, address);
+                read(file_line, bus_read);
+                read(file_line, bus_write);
+                read(file_line, data);
+                
+                wait for delta;
+                
+                apu_bus.address <= address;
+                apu_bus.read <= bus_read;
+                apu_bus.write <= bus_write;
+                apu_data_out <= data;
+            end loop;
             
-            apu_bus.address <= address;
-            apu_bus.read <= bus_read;
-            apu_bus.write <= bus_write;
-            apu_data_out <= data;
-        end loop;
-        file_close(fptr);
-        done <= true;
+            file_close(fptr);
+            done_sig <= true;
+        end if;
     end process;
 
 end behavioral;
