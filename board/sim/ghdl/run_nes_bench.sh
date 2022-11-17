812eda34c5213265df4a86f37a07516fe6fe3954
aarmono <33583235+aarmono@users.noreply.github.com>
2022-11-16 22:20:40 -0800

    Make output directory optional argument to run_nes_bench

--- board/sim/ghdl/run_nes_bench.sh
+++ board/sim/ghdl/run_nes_bench.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
 
 NES_FILEPATH="$1"
-OUTPUT_DIR="$2"
+if [ "$2" != "" ]
+then
+    OUTPUT_DIR="$2"
+    shift 2
+else
+    OUTPUT_DIR=`basename "$1"`
+    shift 1
+fi
 AU_FILEPATH="$OUTPUT_DIR"/audio.au
 BMP_PREFIX="$OUTPUT_DIR"/frame
-NES_SIZE=`wc -c "$1" | cut -d ' ' -f 1`
+NES_SIZE=`wc -c "$NES_FILEPATH" | cut -d ' ' -f 1`
 
 mkdir -p "$OUTPUT_DIR"
 rm -f "$OUTPUT_DIR"/*
-
-shift 2
 
 make -f nes_bench.Makefile &&
 exec ./nes_bench -gAU_FILEPATH="$AU_FILEPATH"    \
