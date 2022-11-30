debdd04a598bafd19411a97859275f4c5102a6b3
aarmono <33583235+aarmono@users.noreply.github.com>
2022-11-29 17:50:19 -0800

    Run tests in parallel

--- board/sim/ghdl/run_nes_tests.sh
+++ board/sim/ghdl/run_nes_tests.sh
@@ -1,26 +1,51 @@
 #!/usr/bin/env bash
 
+check_hash() {
+    echo "$1" | sha256sum -c --status --quiet
+}
+
+run_nes_test() {
+    TEST_PATH=`echo "$1" | cut -f 1 -d '|' | xargs`
+    NES_FILEPATH="$BASEDIR"/"$TEST_PATH"
+    RUN_TIME=`echo "$1" | cut -f 2 -d '|' | xargs`
+    HASH=`echo "$1" | cut -f 3 -d '|' | xargs`
+    OUTPUT_DIR=output/"$TEST_PATH"
+
+    echo "Test starting: $TEST_PATH"
+    ./run_nes_bench.sh -n "$NES_FILEPATH" -o "$OUTPUT_DIR" -t "$RUN_TIME" 1> /dev/null
+
+    if [ "$HASH" != "" ]
+    then
+        pushd "$OUTPUT_DIR" &> /dev/null
+        if check_hash "$HASH"
+        then
+            STATUS=PASS
+        else
+            STATUS=FAIL
+        fi
+
+        echo "Test $STATUS: $TEST_PATH"
+        popd &> /dev/null
+    else
+        echo "Test complete: $TEST_PATH"
+    fi
+}
+
 IFS='
 '
 
 BASEDIR=../../../nes-test-roms
 
 rm -rf output/*
+
+N=`nproc`
 for line in `grep -v '^#' nes_tests.txt`
 do
-    TEST_PATH=`echo "$line" | cut -f 1 -d '|' | xargs`
-    NES_FILEPATH="$BASEDIR"/"$TEST_PATH"
-    RUN_TIME=`echo "$line" | cut -f 2 -d '|' | xargs`
-    HASH=`echo "$line" | cut -f 3 -d '|' | xargs`
-    OUTPUT_DIR=output/"$TEST_PATH"
+    run_nes_test "$line" &
 
-    echo "Running test $TEST_PATH"
-    ./run_nes_bench.sh -n "$NES_FILEPATH" -o "$OUTPUT_DIR" -t "$RUN_TIME" 1> /dev/null
-
-    if [ "$HASH" != "" ]
+    if [ $(jobs -r -p | wc -l) -ge $N ]
     then
-        pushd "$OUTPUT_DIR" &> /dev/null
-        echo "$HASH" | sha256sum -c
-        popd &> /dev/null
+        wait -n
     fi
 done
+wait
